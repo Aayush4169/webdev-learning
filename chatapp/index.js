fb3df5071011fb8c93de7ba1a6555dd0383e14b7
Aayush <aayushahir4169yadav@gmail.com>
2026-06-15 16:54:13 +0530

    error handling try-catch block

--- chatapp/index.js
+++ chatapp/index.js
@@ -28,67 +28,92 @@ app.get("/", (req, res) => {
   res.send("root is working");
 });
 // index route showing all the chatsss
-app.get("/chats", async (req, res) => {
-  const chats = await chat.find();
-  res.render("show.ejs", { chats });
+app.get("/chats", async (req, res, next) => {
+  try {
+    const chats = await chat.find();
+    res.render("show.ejs", { chats });
+  } catch (err) {
+    next(err);
+  }
 });
 
 //  add new chat form
-app.get("/chats/new", (req, res) => {
+app.get("/chats/new", (req, res, next) => {
   res.render("newdata.ejs");
 });
 
 //add new chat
-app.post("/chats", async (req, res) => {
-  let { from, msg, to } = req.body;
-  let newChat = new chat({
-    from: from,
-    msg: msg,
-    to: to,
-    create_at: new Date(),
-  });
-  await newChat.save();
+app.post("/chats", async (req, res, next) => {
+  try {
+    let { from, msg, to } = req.body;
+    let newChat = new chat({
+      from: from,
+      msg: msg,
+      to: to,
+      created_at: new Date(),
+    });
 
-  res.redirect("/chats");
+    await newChat.save();
+
+    res.redirect("/chats");
+  } catch (err) {
+    next(err);
+  }
 });
 
 //  this route open a form for edit thechat
-app.get("/chats/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  let edchat = await chat.findById(id);
+app.get("/chats/:id/edit", async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let edchat = await chat.findById(id);
 
-  res.render("edit.ejs", { edchat });
+    res.render("edit.ejs", { edchat });
+  } catch (err) {
+    next(err);
+  }
 });
 
 // update the edit the chatt
-app.put("/chats/:id", async (req, res) => {
-  let { id } = req.params;
-  let { msg } = req.body;
-  let updatechat = await chat.findByIdAndUpdate(
-    id,
-    { msg: msg, updated_at: new Date() },
-    { runValidators: true, new: true },
-  );
-  console.log(updatechat);
-  res.redirect("/chats");
+app.put("/chats/:id", async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let { msg } = req.body;
+    let updatechat = await chat.findByIdAndUpdate(
+      id,
+      { msg: msg, updated_at: new Date() },
+      { runValidators: true, new: true },
+    );
+    console.log(updatechat);
+    res.redirect("/chats");
+  } catch (err) {
+    next(err);
+  }
 });
 // destroy the chat
-app.delete("/chats/:id", async (req, res) => {
-  let { id } = req.params;
-  let delchat = await chat.findByIdAndDelete(id);
-  console.log(delchat);
-  res.redirect("/chats");
+app.delete("/chats/:id", async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let delchat = await chat.findByIdAndDelete(id);
+    console.log(delchat);
+    res.redirect("/chats");
+  } catch (err) {
+    next(err);
+  }
 });
 
 //  new route for error handlingggg
 
 app.get("/chats/:id", async (req, res, next) => {
-  let { id } = req.params;
-  let schat = await chat.findById(id);
-  if (!schat) {
-    next(new expressError(500, "chat not found"));
+  try {
+    let { id } = req.params;
+    let schat = await chat.findById(id);
+    // if (!schat) {
+    //   next(new expressError(500, "chat not found"));
+    // }
+    res.render("edit.ejs", { schat });
+  } catch (err) {
+    next(err);
   }
-  res.send(schat);
 });
 app.use((err, req, res, next) => {
   let { status = 400, message = "not found" } = err;
